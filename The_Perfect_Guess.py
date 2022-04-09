c9a89f1f47cb936b44339effffc8b3225bc9075a
KingSenpai <91060229+ChintuWhitehatjunior@users.noreply.github.com>
2022-04-09 16:45:39 +0530

    Update The_Perfect_Guess.py

--- The_Perfect_Guess.py
+++ The_Perfect_Guess.py
@@ -3,13 +3,14 @@ import random
 randNo = random.randint(1,100)
 ans = None
 guess = 0
+hiscore = 100
 try:
     while(ans != randNo):
         ans = int(input("Enter your guess:- "))          
         if(ans == randNo):
             guess += 1
             print("You Guessed Right!")
-            if(guess>1):
+            if(ans == 1):
                 print(f"It took you a single guess, Amazing!")
             else:
                 print(f"It took you {guess} guesses")
@@ -22,3 +23,10 @@ try:
                 guess += 1
 except Exception as e:
     print(f"Your input caused an error, please input an integer and try again!")
+
+if(guess<hiscore):
+    hiscore = guess
+    print("You just beat the highscore!")
+    print(f"New Highscore:- {hiscore}")
+else:
+    pass
